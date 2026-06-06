2a5b577eaf9238e43d79267e72dc333d5cc666c7
FFGAMINGRAJ <98765020+FFGAMINGRAJ@users.noreply.github.com>
2026-06-06 15:40:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,8 +55,8 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '-1003494905453')  # Request cha
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Anime_Group_Chatts')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
-auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003252280306,-1003494905453")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-1003335865847,-1003361913043")# Channels for force sub (make sure bot is admin)
+auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003252280306 ,-1003494905453")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
+auth_channels     = environ.get("AUTH_CHANNELS", "-1003335865847 ,-1003361913043")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
