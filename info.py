5efe4c2d427aa22e52ca5a476be51e8f1b40cbb5
FFGAMINGRAJ <98765020+FFGAMINGRAJ@users.noreply.github.com>
2026-06-06 19:33:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -94,9 +94,11 @@ TMDB_API_KEY = environ.get('TMDB_API_KEY', '64ac243c746e85ce73137ab9a1c0f095') #
 TMDB_POSTER = bool(environ.get('TMDB_POSTER', True)) # Shows TMDB poster in notification msg
 LANDSCAPE_POSTER = bool(environ.get('LANDSCAPE_POSTER', True)) # Shows landscape poster in notification msg
 
+MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+
 # Poster settings
 DREAMXBOTZ_IMAGE_FETCH = True
-TMDB_API_KEY = ""
+TMDB_API_KEY = "64ac243c746e85ce73137ab9a1c0f095"
 
 POSTER_SPOILER= bool(environ.get('POSTER_SPOILER', True))  # On (True) / Off (False)
 
