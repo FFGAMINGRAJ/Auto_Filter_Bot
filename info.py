12ced323cdda53b2d025c549e05f6e9a799b91a3
FFGAMINGRAJ <98765020+FFGAMINGRAJ@users.noreply.github.com>
2026-06-08 15:31:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -122,7 +122,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Animes_Request_Group') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+c3XoY1EmVMBmYWFl') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Rz_Anime_Group') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/+bsUOyWoWles0ZmJl') # Update channel link for the bot
 
