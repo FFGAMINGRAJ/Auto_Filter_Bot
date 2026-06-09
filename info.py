8c02cb33aa1a36f78bf2ad9d55bd974fb0d2f910
FFGAMINGRAJ <98765020+FFGAMINGRAJ@users.noreply.github.com>
2026-06-09 12:29:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,7 +62,7 @@ auth_channels     = environ.get("AUTH_CHANNELS", "-1003335865847 -1003361913043"
 # Payment Configuration
 # ============================
 QR_CODE = environ.get('QR_CODE', 'https://i.ibb.co/kgrwDqTZ/x.jpg')    # QR code image for payments
-OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'ɴᴏ ᴀᴠᴀɪʟᴀʙʟᴇ ʀɪɢʜᴛ ɴᴏᴡ')    # Owner UPI ID for payments
+OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'mikeykunraj@fam')    # Owner UPI ID for payments
 
 STAR_PREMIUM_PLANS = {
     10: "7day",
